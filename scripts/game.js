97f7cef9b8679aec501f490e7dbf2258a5f80957
Claudio Cruz <claudiocruz1989@gmail.com>
2022-08-31 05:04:13 +0000

    First commit

---
+++ scripts/game.js
@@ -0,0 +1,18 @@
+let game = {
+    currentGame: [],
+    playerMoves: [],
+    score: 0,
+    choices: ["button1", "button2", "button3", "button4"]
+};
+
+function newGame() {
+    game.score = 0;
+    game.playerMoves = [];
+    game.currentGame = [];
+};
+
+function showScore() {
+    document.getElementById('score').innerText = game.score;
+};
+
+module.exports = { game, newGame, showScore };
